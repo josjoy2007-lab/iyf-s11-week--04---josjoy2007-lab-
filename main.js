c51ed765f684785e8cee4a5582878715c69e930f
josjoy2007-lab <josjoy2007@gmail.com>
2026-07-07 14:00:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+//Variable description
+let name="Josphine Wairimu";
+console.log(name)
+let age=18;
+console.log(age)
+const status=student;
+console.log(student)
+let new date=July 7,2026
+console.log(new date)
+
 
